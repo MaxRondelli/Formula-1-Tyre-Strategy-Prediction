8e0291f118eb69a7e79ab456ac877d1b1d527b31
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2023-01-03 11:38:25 +0100

    Update main.py
    Added weather conditions on dataframe.

--- main.py
+++ main.py
@@ -7,20 +7,26 @@ ff1.Cache.enable_cache('Cache') # The argument is the name of the folder.
 
 # Choose session and driver
 session = ff1.get_session(2022, 'Imola', 'R') # Imola's race in 2022
-driver = 'LEC'  
+driver = 'VER'  
 
 session.load() # Load the session
 
 laps_driver = session.laps.pick_driver(driver) 
 
+# Lap features
 lap_time = laps_driver['LapTime'] # Session time when the lap was set (End of lap).
 lap_numbers = laps_driver['LapNumber'] #  That's our temporal resolution T. It's effective lenght of one unit of time. 
+
+# Tyre features
 tyre_life = laps_driver['TyreLife'] # Laps driven on this tyre. It includes laps in other session for used sets of tyre.
 compound = laps_driver['Compound'] # Tyre compound (SOFT, MEDIUM, HARD, INTERMEDIATE, WET)
+stint = laps_driver['Stint'] # Stint number
 
+# Weather conditions features
+weather_rainfall = session.laps.get_weather_data()['Rainfall'] # Shows if there is rainfall
+weather_track_temperature = session.laps.get_weather_data()['TrackTemp'] # Track temperature [°C]
 
 # Create DataFrame
-list_of_tuples = list(zip(lap_time, lap_numbers, tyre_life, compound))
-df = pd.DataFrame(list_of_tuples, columns = ['Lap', 'Time', 'Compound', 'Tyre Life'])
+list_of_tuples = list(zip(lap_time, lap_numbers, tyre_life, compound, stint, weather_rainfall, weather_track_temperature))
+df = pd.DataFrame(list_of_tuples, columns = ['Lap', 'Time', 'Compound', 'Tyre Life', 'Stint', 'Rainfall', 'Track Temp'])
 print(df.to_markdown())
-
