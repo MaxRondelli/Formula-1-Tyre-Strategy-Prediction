d59f56eb7a0ba594182126c0592e1b5418d1f418
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2023-01-06 01:26:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ session.load() # Load the session
  
 # Lap features
 laps_driver = session.laps.pick_driver(driver) 
-lap_time = laps_driver['LapTime'] # Session time when the lap was set (End of lap).
+lap_time = laps_driver['LapTime'] # Session time when the lap was set (End of lap) for the specific driver. 
 lap_numbers = laps_driver['LapNumber'].to_numpy() # That's our temporal resolution T. It's effective lenght of one unit of time. 
 
 # Tyre features
@@ -45,20 +45,18 @@ def get_lap_times():
             # Create a new dictionary for each tuple
             lap_dict = {'driver': driver, 'lap': count ,'lap_time': lap}
             
-            # Add the dictionary to the list
-            lap_time_data_dict.append(lap_dict)
+            if pd.notnull(lap): # Value != NaT. The first lap is not in the dict. 
+                # Add the dictionary to the list
+                lap_time_data_dict.append(lap_dict)
             
             # Update count
             count = count + 1 
                     
     return lap_time_data_dict
 
-print(get_lap_times())   
-
 # Create DataFrame
 list_of_tuples = list(zip(lap_time, lap_numbers, tyre_life, compound, stint, weather_rainfall, weather_track_temperature))
 df = pd.DataFrame(list_of_tuples, columns = ['Lap', 'Time', 'Compound', 'Tyre Life', 'Stint', 'Rainfall', 'Track Temp'])
-#print(df.to_markdown())
 
 # One-hot encode the categorical columns
 df_encoded = pd.get_dummies(df)
@@ -66,23 +64,23 @@ df_encoded = pd.get_dummies(df)
 # Convert the encoded dataframe to a NumPy Array
 array = df_encoded.to_numpy()
 
-# Right now we have to understand how to build y. 
-# y is a vector that contains [y_1, . . . , y_t]. At t time it has to contain the value of the best tyre for that time. 
-# We gotta find out the metric system. The metric system is a characteristics that make a specific tyre better than another. 
-
+# ------ Create y output vector ------
 metric = lap_numbers # Determine the metric to use for choosing the best driver
 y = [] # Output vector 
 
 # Function choose driver with the best lap time among those available at t time, using the lap time data provided as input.
-# Basically it return the winner of the race. 
 def choose_driver(lap_data_dict):
-    best_driver = lap_data_dict[0]["driver"]
-    best_lap_time = lap_data_dict[0]["lap_time"]
+    best_driver = lap_data_dict[0]['driver']
+    best_lap_time = lap_data_dict[0]['lap_time']
 
     for driver in lap_data_dict:
         # If this driver's lap time is better than the best lap time of the current best driver, update the best driver.
-        if driver["lap_time"] < best_lap_time:
-            best_driver = driver["driver"]
-            best_lap_time = driver["lap_time"]
+        if driver['lap_time'] < best_lap_time:
+            best_lap_time = driver['lap_time']
+            best_driver = driver['driver']
+            
+    return best_driver, best_lap_time
 
-    return best_driver
+lap_data = get_lap_times()
+best_lap = choose_driver(lap_data)
+print(best_lap)
