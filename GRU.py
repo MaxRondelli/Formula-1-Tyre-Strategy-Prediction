e3a4a37de77bdd1b56bc16c44b642c241b7627e3
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2023-02-15 15:16:00 +0100

    Create GRU network

---
+++ GRU.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras.layers import GRU, Dropout, Dense
+from keras.models import Sequential 
+from utils import *
+import sys 
+
+np.set_printoptions(threshold=sys.maxsize)
+
+dataset = np.load('data.npy')
+
+X = dataset[:, :, :-1]
+y = dataset[:, :, -1]
+
+def train_split(X, y, p):
+    N = len(X)
+    n_train = int(N*p)
+    
+    x_train = X[:n_train]
+    y_train = y[:n_train]
+    x_test = X[n_train:]
+    y_test = y[n_train:]
+
+    return (x_train, y_train), (x_test, y_test)
+
+(x_train, y_train), (x_test, y_test) = train_split(X, y, 0.8)
+y_train = tf.keras.utils.to_categorical(y_train, 5)
+y_test = tf.keras.utils.to_categorical(y_test, 5)
+
+model = Sequential()
+
+model.add(GRU(128, input_shape = (x_train.shape[1:]), activation = 'relu', return_sequences = True))
+model.add(Dropout(0.2))
+
+model.add(GRU(128, activation = 'relu', return_sequences = True))
+# model.add(Dropout(0.2))
+
+# model.add(LSTM(128, activation = 'relu', return_sequences = True))
+# model.add(Dropout(0.2))
+
+model.add(GRU(5, input_shape = (x_train.shape[1:]), activation = 'softmax', return_sequences = True))
+
+opt = tf.keras.optimizers.Adam(learning_rate = 5e-4)
+
+model.compile(loss = 'mse', 
+              optimizer = opt, 
+              metrics = ['accuracy'])
+      
+print(model.summary())
+# Train the model
+model.fit(x_train, y_train, epochs = 1000, shuffle = False)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+# Save the model
+model.save('gru_epochs()_acc()_loss().h5')
