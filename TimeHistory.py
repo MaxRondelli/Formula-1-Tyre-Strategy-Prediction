affaf1677d251297110ef3e460e02cbdb6a39d03
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2023-02-16 15:37:05 +0100

    Create time class for total training time

---
+++ TimeHistory.py
@@ -0,0 +1,11 @@
+import time
+from keras.callbacks import Callback
+
+class TimeHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.train_time_start = time.time()
+
+    def on_train_end(self, logs={}):
+        self.train_time_end = time.time()
+        self.total_train_time = self.train_time_end - self.train_time_start
+        print('Total training time: {:.2f} seconds'.format(self.total_train_time))
