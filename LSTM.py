6d2c34454ca40f605411dae45c1544d2102c669b
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2023-02-17 18:57:13 +0100

    Add random classificator

--- LSTM.py
+++ LSTM.py
@@ -7,61 +7,76 @@ from utils import *
 import sys 
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 
-np.set_printoptions(threshold=sys.maxsize)
+# np.set_printoptions(threshold=sys.maxsize)
 
-dataset = np.load('data.npy')
+# dataset = np.load('data.npy')
+# print(dataset.shape)
 
-X = dataset[:, :, :-1]
-y = dataset[:, :, -1]
+# X = dataset[:, :, :-1]
+# y = dataset[:, :, -1]
 
-def train_split(X, y, p):
-    N = len(X)
-    n_train = int(N*p)
+# def train_split(X, y, p):
+#     N = len(X)
+#     n_train = int(N*p)
     
-    x_train = X[:n_train]
-    y_train = y[:n_train]
-    x_test = X[n_train:]
-    y_test = y[n_train:]
+#     x_train = X[:n_train]
+#     y_train = y[:n_train]
+#     x_test = X[n_train:]
+#     y_test = y[n_train:]
 
-    return (x_train, y_train), (x_test, y_test)
+#     return (x_train, y_train), (x_test, y_test)
 
-(x_train, y_train), (x_test, y_test) = train_split(X, y, 0.8)
-y_train = tf.keras.utils.to_categorical(y_train, 5)
-y_test = tf.keras.utils.to_categorical(y_test, 5)
+# (x_train, y_train), (x_test, y_test) = train_split(X, y, 0.8)
+# y_train = tf.keras.utils.to_categorical(y_train, 5)
+# y_test = tf.keras.utils.to_categorical(y_test, 5)
 
-model = Sequential()
+# '''
+# p_i = calcolare la probabilità di ogni compound all'interno di y_train 
 
-model.add(LSTM(128, input_shape = (x_train.shape[1:]), activation = 'relu', return_sequences = True))
-model.add(Dropout(0.2))
+# p_0 = np.sum((y_train == 0)) / len(y_train)
+# n_0 = np.sum((y_test == 0))
 
-model.add(LSTM(128, activation = 'relu', return_sequences = True))
+# '''
+# for i in y_train:
+#     p_i = np.sum((y_train == i)) / len(y_train)
+#     n_i = np.sum((y_train == i))
+    
+# random_classificator = (p_i * n_i) / n_i
+
+
+# model = Sequential()
+
+# model.add(LSTM(128, input_shape = (x_train.shape[1:]), activation = 'relu', return_sequences = True))
 # model.add(Dropout(0.2))
-model.add(LSTM(128, activation = 'relu', return_sequences = True))
-
-model.add(LSTM(128, activation = 'relu', return_sequences = True))
 
 # model.add(LSTM(128, activation = 'relu', return_sequences = True))
-# model.add(Dropout(0.2))
+# # model.add(Dropout(0.2))
+# model.add(LSTM(128, activation = 'relu', return_sequences = True))
 
-model.add(LSTM(5, input_shape = (x_train.shape[1:]), activation = 'softmax', return_sequences = True))
+# model.add(LSTM(128, activation = 'relu', return_sequences = True))
 
-opt = tf.keras.optimizers.Adam(learning_rate = 5e-4)
+# # model.add(LSTM(128, activation = 'relu', return_sequences = True))
+# # model.add(Dropout(0.2))
 
-model.compile(loss = 'mse', 
-              optimizer = opt, 
-              metrics = ['accuracy'])
+# model.add(LSTM(5, input_shape = (x_train.shape[1:]), activation = 'softmax', return_sequences = True))
+
+# opt = tf.keras.optimizers.Adam(learning_rate = 5e-4)
+
+# model.compile(loss = 'mse', 
+#               optimizer = opt, 
+#               metrics = ['accuracy'])
       
-# Checkpoint and early stop
-checkpoint = ModelCheckpoint("model_weight_lstm.h5", save_best_only=True, save_weights_only=True, monitor='loss', mode='min', verbose=1)
-early_stop = EarlyStopping(monitor='loss', patience=5, mode='min', verbose=1)
+# # Checkpoint and early stop
+# checkpoint = ModelCheckpoint("model_weight_lstm.h5", save_best_only=True, save_weights_only=True, monitor='loss', mode='min', verbose=1)
+# early_stop = EarlyStopping(monitor='loss', patience=10, mode='min', verbose=1)
 
-# Train the model
-time_callback = TimeHistory()
-model.fit(x_train, y_train, epochs = 10, shuffle = False, callbacks=[checkpoint, early_stop, time_callback])
+# # Train the model
+# time_callback = TimeHistory()
+# model.fit(x_train, y_train, epochs = 100, shuffle = False, callbacks=[checkpoint, early_stop, time_callback])
 
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
-
-# Save the model
-model.save('lstm_epochs()_acc()_loss().h5')
+# score = model.evaluate(x_test, y_test, verbose=0)
+# print('Random classificator:', random_classificator)
+# print('Test loss:', score[0])
+# print('Test accuracy:', score[1])
+# # Save the model
+# model.save('lstm_epochs()_acc()_loss().h5')
