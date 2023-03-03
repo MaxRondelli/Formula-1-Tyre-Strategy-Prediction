fa7a32cbd5915f525854cf7ba70b0211418896ae
MaxRondelli <83310259+MaxRondelli@users.noreply.github.com>
2023-03-03 11:05:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,13 @@
 import fastf1 as ff1
-import pandas as pd
-import numpy as np
 from utils import *
-from LSTM import *
 
 # Enable fastf1 cache
 ff1.Cache.enable_cache('Cache')
 
 #--------------- Generate .npy file for the first experiment ---------------
-# year_list = [2022]
-# x = load_dataset(year_list)
-# print(x)
+year_list = [2021, 2022]
+exp1_data = load_dataset(year_list)
 
 #--------------- Generate .npy file for the second experiment ---------------
-# session = ff1.get_session(2019, 'Imola', 'R')
-# session.load()
-# driver = 'HAM'
-
-# driver_session = session.laps.pick_driver(driver)
-# compound = driver_session['Compound']
-# print(compound)
-# driver_list = pd.unique(session.laps['Driver'])
-# print(driver_list)
-
-# year_list = [2019, 2020, 2021, 2022]
-# # for year in year_list:
-# #     df = get_information(session, driver, year)
-# #     print(df.to_markdown())
-    
-# x = get_dataset(year_list)
-# print(x)
-
-# # grand_prix_list = ff1.get_event_schedule(2020)
-# # print(grand_prix_list)
-
-# print(get_race_list(2020))
+year_list = [2019, 2020, 2021, 2022]
+exp2_final_data = get_dataset(year_list)
