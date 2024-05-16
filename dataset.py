40216cf8a346b43bb3511d6fd226770a35d1d178
MaxRondelli <massi.rondelli@gmail.com>
2024-05-16 12:27:34 +0200

    Feat: set target as last column

--- dataset.py
+++ dataset.py
@@ -11,19 +11,33 @@ ff1.Cache.enable_cache('cache')
 
 # Lists to store data for DataFrame
 def fastestDriverData(lap):
+    COMPOUNDS = {"SOFT": 0, "MEDIUM": 1, "HARD": 2, "INTERMEDIATE": 3, "WET": 4} # Compounds dictionary. It is necessary to convert compound string to int
+    
     with warnings.catch_warnings():
         warnings.filterwarnings("ignore", message="In the future, `None` will be returned instead of an empty `Lap` object")
         compound = session.laps.pick_lap(lap).pick_fastest()['Compound']
 
         if not isinstance(compound, float) or not math.isnan(compound):
+            # Enconding the compound from string to int
+            if compound is not None:
+                compound = COMPOUNDS[compound]
+            elif compound is np.NaN or compound is None:
+                compound = -1 
+            if compound == -1:
+                print(f"Warning: Compound '{compound}' not found in COMPOUNDS dictionary")
+            
+            # Weather data
             weather_data = session.laps.pick_lap(lap).pick_fastest().get_weather_data()
             weather_data = weather_data[['AirTemp', 'Humidity', 'Pressure', 'Rainfall', 'TrackTemp', 'WindDirection', 'WindSpeed']]
+
+            if weather_data['Rainfall'] == True:
+                weather_data['Rainfall'] = 1
+            else: 
+                weather_data['Rainfall'] = 0
+
             # Format Time 
             lap_time = session.laps.pick_lap(lap).pick_fastest()['LapTime']
-            minutes = lap_time.seconds // 60
-            seconds = lap_time.seconds % 60
-            milliseconds = lap_time.microseconds // 1000
-            lap_time = f"{minutes:02d}:{seconds:02d}.{milliseconds:03d}"
+            lap_time = pd.to_timedelta(lap_time).total_seconds() # convert lap time from api format to seconds
 
             return compound, weather_data, lap_time
 
@@ -43,7 +57,6 @@ def createDataframe(total_laps):
             # Construct lap dictionary
             lap_dict = {
                 'LAP': lap,
-                'COMPOUND': compound,
                 'AIR TEMP': weather_data.get('AirTemp', ""),
                 'HUMIDITY': weather_data.get('Humidity', ""),
                 'PRESSURE': weather_data.get('Pressure', ""),
@@ -51,9 +64,10 @@ def createDataframe(total_laps):
                 'TRACK TEMP': weather_data.get('TrackTemp', ""),
                 'WIND DIRECTION': weather_data.get('WindDirection', ""),
                 'WIND SPEED': weather_data.get('WindSpeed', ""),
-                'LAP TIME': lap_time
+                'LAP TIME': lap_time,
+                'COMPOUND': compound
             }
-            
+
             # Append lap data to the list
             lap_data.append(lap_dict)
     
@@ -72,9 +86,8 @@ def createDataframe(total_laps):
         combined_df = df
     combined_df.to_csv('lap_data.txt', sep='\t', index=False, quoting=csv.QUOTE_NONE)
 
-    # npy code 
-    # Check if the .npy file exists
-    npy_file_exists = os.path.isfile('lap_data.npy')
+    # Numpy file 
+    npy_file_exists = os.path.isfile('lap_data.npy') # Check if the .npy file exists
 
     # If the .npy file exists, load the existing data
     if npy_file_exists:
